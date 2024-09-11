06c4e544bb2bb6fd3406dade097a9662cda8c7fa
whutxd <whutxd@163.com>
2024-09-11 14:51:43 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,8 @@ export function SideBar(props: { className?: string }) {
           FDM ChatGPT
         </div>
         {/* 【已注释】<div className={styles["sidebar-sub-title"]} style={{ color: 'orange', fontWeight: 'bold', fontStyle: 'italic' }}> */}
-        <div className={styles["sidebar-sub-title"]} style={{ color: 'pink', fontWeight: 'bold', fontStyle: 'normal' }}>
+        {/* 【已注释】<div className={styles["sidebar-sub-title"]} style={{ color: 'pink', fontWeight: 'bold', fontStyle: 'normal' }}> */}
+        <div className={styles["sidebar-sub-title"]} style={{ color: '#FF5A5D', fontWeight: 'normal', fontStyle: 'normal' }}>
           Welcome!<br />
           你好，我是方正电机开发的ChatGPT应用.<br />
           2024.7.15：<br />
@@ -167,7 +168,7 @@ export function SideBar(props: { className?: string }) {
           2024.7.22：<br />
           - 默认模型升级为gpt-4o-mini(OpenAI)<br />
           {/* 【已注释】2024.9.11：<br /> */}
-          <span style={{ color: 'pink', fontWeight: 'bold', fontStyle: 'italic' }}>2024.9.11：</span><br />
+          <span style={{ color: '#FF5A5D', fontWeight: 'bold', fontStyle: 'italic' }}>2024.9.11：</span><br />
           - <a href="https://navi.fdmgpt.top" target="_blank" style={{ color: '#FF5A5D', fontWeight: 'bold', fontStyle: 'italic' }}>效率工具和数字资源库导航</a><br />
           - <a href="http://fdmai.top" target="_blank" style={{ color: '#FF5A5D', fontWeight: 'bold', fontStyle: 'italic' }}>FDM AI平台</a><br />
           - <a href="https://chatgptplus.cn" target="_blank" style={{ color: '#FF5A5D', fontWeight: 'bold', fontStyle: 'italic' }}>ChatGPT PLUS公益站点</a>
