ca32d0c30f7cf18148b2556ee2c9b238f2da3559
whutxd <whutxd@163.com>
2024-09-11 14:45:17 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -168,9 +168,9 @@ export function SideBar(props: { className?: string }) {
           - 默认模型升级为gpt-4o-mini(OpenAI)<br />
           {/* 【已注释】2024.9.11：<br /> */}
           <span style={{ color: 'pink', fontWeight: 'bold', fontStyle: 'italic' }}>2024.9.11：</span><br />
-          - <a href="https://navi.fdmgpt.top" target="_blank" style={{ color: 'pink', fontWeight: 'bold', fontStyle: 'italic' }}>效率工具和数字资源库导航</a><br />
-          - <a href="http://fdmai.top" target="_blank" style={{ color: 'pink', fontWeight: 'bold', fontStyle: 'italic' }}>FDM AI平台</a><br />
-          - <a href="https://chatgptplus.cn" target="_blank" style={{ color: 'pink', fontWeight: 'bold', fontStyle: 'italic' }}>ChatGPT PLUS公益站点</a>
+          - <a href="https://navi.fdmgpt.top" target="_blank" style={{ color: '#FF5A5D', fontWeight: 'bold', fontStyle: 'italic' }}>效率工具和数字资源库导航</a><br />
+          - <a href="http://fdmai.top" target="_blank" style={{ color: '#FF5A5D', fontWeight: 'bold', fontStyle: 'italic' }}>FDM AI平台</a><br />
+          - <a href="https://chatgptplus.cn" target="_blank" style={{ color: '#FF5A5D', fontWeight: 'bold', fontStyle: 'italic' }}>ChatGPT PLUS公益站点</a>
 
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
